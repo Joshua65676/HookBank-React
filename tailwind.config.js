45384085da6d4968cf24ce4b0c339ee4b9a7f46a
Okoro Joshua <Joshua65676@gmail.com>
2024-05-21 14:33:11 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         brightBlack: 'rgba(39, 38, 40, 255)',
